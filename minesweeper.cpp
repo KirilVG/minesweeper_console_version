96a525e5e428dad070e5bee9b7ea9a6fe41ed147
KirilVG <96527486+KirilVG@users.noreply.github.com>
2021-12-22 14:15:07 +0200

    Create minesweeper.cpp
    declare variables

---
+++ minesweeper.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+void playGame()
+{
+    //declaring variables
+    const int mapSize = 9;
+    int numberOfBombs = 10;
+    srand((unsigned)time(0));
+    char map[mapSize][mapSize];
+    char visibleMap[mapSize][mapSize];
+}
+
+int main()
+{
+    playGame();
+
+    return 0;
+}
